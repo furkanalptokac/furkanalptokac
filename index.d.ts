408d6efc52d23bead371ea7a32e3cfba27ecbb72
Furkan Alp Tokaç <furkanalptokac@gmail.com>
2025-08-30 23:25:03 +0300

    feat: update personal information, update version

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,7 @@
 export declare const me: {
-  job: string;
+  bsc: string;
+  position: string;
   website: string;
   github: string;
-  currentlyWorkingOn: string;
-  interests: {
-      1: string;
-      2: string;
-      3: string;
-      4: string;
-      5: string;
-  };
+  stack: string;
 };
--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 #!/usr/bin/env node
 
 export const me = {
-    job: "Computer Engineer",
+    bsc: 'Computer Engineer',
+    position: 'Full Stack JavaScript Developer',
     website: "https://furkanalp.com",
     github: "https://github.com/furkanalptokac",
-    currentlyWorkingOn: "Frontend based w/ TypeScript + React.",
-    interests: {
-        1: 'Full stack with JS technologies.',
-        2: 'Competitive programming.',
-        3: 'Pixel art, UI/UX design, graphic design, illustration... w/ Figma, Zeplin, Sketch, XD, PS, AI...',
-        4: 'Electronic music producer & composer w/ FL Studio, Ableton, Logic Pro...',
-        5: 'Former level designer. Still playing video games & chess.',
-    },
+    stack: "React, Next, Tailwind, Angular, Vue, Node, Rollup, Vite, Docker",
 };
 
 console.log(me);
